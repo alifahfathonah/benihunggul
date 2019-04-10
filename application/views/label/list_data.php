ae310e719baf2aea228ca2371cae57c9897a24e3
oktavsyh <42313230+oktavsyh@users.noreply.github.com>
2019-04-10 16:03:12 +0700

    Update list_data.php

--- application/views/label/list_data.php
+++ application/views/label/list_data.php
@@ -9,7 +9,6 @@
       <td class="text-center" style="min-width:150px;">
         <button class="btn btn-warning update-dataLabel" data-id="<?php echo $label->id_label; ?>"><i class="glyphicon glyphicon-edit"></i></button>
         <button class="btn btn-danger konfirmasiHapus-label" data-id="<?php echo $label->id_label; ?>" data-toggle="modal" data-target="#konfirmasiHapus"><i class="glyphicon glyphicon-remove-sign"></i></button>
-        <button class="btn btn-info detail-dataLabel" data-id="<?php echo $label->id_label; ?>"><i class="glyphicon glyphicon-info-sign"></i></button>
       </td>
     </tr>
     <?php
